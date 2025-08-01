1ea5113e7f2e5cc777d347388060c0f735d12308
fiston <kngfizzy6@gmail.com>
2025-08-01 09:38:45 +0200

    initial commit with DB intro project

---
+++ MySQLServer.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+def create_database():
+    try:
+        # Connect to MySQL Server
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",              
+            password="Sandokan05@@"   
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except mysql.connector.Error as err:
+        print(f"Error while connecting to MySQL: {err}")
+
+    finally:
+        if 'connection' in locals() and connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
+
+if __name__ == "__main__":
+    create_database()
